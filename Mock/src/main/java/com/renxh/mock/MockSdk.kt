a0bcda0337d8e4f961a070eddf65a2cbf903b0bc
renxiaohui <ren.xiaohui@immomo.com>
2022-09-02 18:59:55 +0800

    添加悬浮窗

--- Mock/src/main/java/com/renxh/mock/MockSdk.kt
+++ Mock/src/main/java/com/renxh/mock/MockSdk.kt
@@ -1,7 +1,12 @@
 package com.renxh.mock
 
+import android.app.Activity
 import android.app.Application
+import android.content.Intent
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.view.WindowManager
 import com.renxh.mock.db.DbUtils
 
 object MockSdk {
@@ -13,5 +18,14 @@ object MockSdk {
         MockServer.init(application)
         val ipAddress = IPConfig.getIpAddress(application)
         Log.d("MockSdk", ipAddress + "/")
+
+
     }
+    fun initService(activity: Activity) {
+        Utils.checkSuspendedWindowPermission(activity) {
+            activity.startService(Intent(activity, SuspendwindowService::class.java))
+        }
+    }
+
+
 }
--- app/src/main/java/com/renxh/androidmock/MainActivity.kt
+++ app/src/main/java/com/renxh/androidmock/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.renxh.androidmock
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.renxh.mock.MockSdk
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        findViewById<TextView>(R.id.textview).setOnClickListener {
+            MockSdk.initService(this)
+        }
     }
 }
